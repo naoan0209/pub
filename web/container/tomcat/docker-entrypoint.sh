197afb8d8b9c13d815b5dbc37de813bc90a3a654
naoan0209 <80740098+naoan0209@users.noreply.github.com>
2024-02-19 21:45:35 +0900

    Add files via upload

--- web/container/tomcat/docker-entrypoint.sh
+++ web/container/tomcat/docker-entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e
 
-# 例
-if [ -z "$MY_ENV_VAR" ]; then
-    export MY_ENV_VAR="default_value"
-fi
-
-# 初期化タスクを実行
-# javaのシステムプロパティをセット
+# 初期化タスクとしてjavaのシステムプロパティをセット
 JAVA_OPTS="$JAVA_OPTS -Dsome.property=value"
 
-# Tomcat を起動する前に環境変数をエクスポート
+# Tomcatを起動する前に環境変数をエクスポート
 export JAVA_OPTS
 
 # コンテナのメインプロセスを実行
