96cfc708d994c9deeb15dd6a18efeb63641f6d94
naoan0209 <80740098+naoan0209@users.noreply.github.com>
2024-05-20 14:10:43 +0900

    Update docker-entrypoint.sh

--- amazonlinux/docker-entrypoint.sh
+++ amazonlinux/docker-entrypoint.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
-# -----------------------------------
-# docker-entrypoint.sh
-# -----------------------------------
 set -e
 
-echo "start $(basename $0)"
-echo "引数を解析します"
-
-echo "=== getopts ==="
-while getopts "a:b:" opt; do
-  case $opt in
-    a)
-      echo "Option -a was specified with value $OPTARG"
-      ;;
-    b)
-      echo "Option -b was specified with value $OPTARG"
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
-  esac
-done
-echo "==============="
-
-echo "end $(basename $0)"
+# コンテナのメインプロセスを実行
+exec "$@"
