aa35fecc47021d70dc9d2eb43a24fc7edd28d07d
naoan0209 <80740098+naoan0209@users.noreply.github.com>
2024-06-13 22:28:29 +0900

    Update s3mount.sh

--- datalink/s3mount.sh
+++ datalink/s3mount.sh
@@ -3,13 +3,13 @@
 BUCKET="s3-mount-977566148511"
 MOUNT_POINT="/mnt"
 
-mkdir -p "$MOUNT_POINT"
+mkdir -p "${MOUNT_POINT}"
 
 # マウントされていたらスキップ
-if mountpoint -q "$MOUNT_POINT"; then
-    echo "$MOUNT_POINT is already mounted."
+if mountpoint -q "${MOUNT_POINT}"; then
+    echo "${MOUNT_POINT} is already mounted."
 else
-    echo "$MOUNT_POINT is not mounted. Proceeding with mount."
+    echo "${MOUNT_POINT} is not mounted. Proceeding with mount."
     # デバイスをマウント
     mount-s3 "${BUCKET}" "${MOUNT_POINT}" --allow-delete
     if [ $? -eq 0 ]; then
