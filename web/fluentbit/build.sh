f2fe936bb585dd1c9a44b337946490dba7660626
naoan0209 <80740098+naoan0209@users.noreply.github.com>
2024-08-04 09:16:42 +0900

    Delete web directory

--- web/fluentbit/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t fluentbit .
-docker tag fluentbit:latest 977566148511.dkr.ecr.ap-northeast-1.amazonaws.com/fluentbit:latest
-docker push 977566148511.dkr.ecr.ap-northeast-1.amazonaws.com/fluentbit:latest
